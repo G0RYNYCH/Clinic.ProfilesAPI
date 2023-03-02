4a6732f6785c2df3f5c03140d5a3a158d2590c0d
G0RYNYCH <92940275+GORYNTCHdotNET@users.noreply.github.com>
2023-03-02 20:06:03 +0300

    Add Entities

---
+++ ProfilesAPI/Models/Account.cs
@@ -0,0 +1,26 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Microsoft.AspNetCore.Identity;
+
+namespace ProfilesAPI.Models;
+
+[ExcludeFromCodeCoverage]
+public class Account : IdentityUser<Guid>
+{
+    public string Email { get; set; }
+
+    public string Password { get; set; }
+
+    public string PhoneNumber { get; set; }
+
+    public bool IsEmailVerified { get; set; }
+
+    public Guid PhotoId { get; set; }
+
+    public Account CreatedBy { get; set; }
+
+    public DateTime CreatedAt { get; set; }
+
+    public Account? UpdatedBy { get; set; }
+
+    public DateTime? UpdatedAt { get; set; }
+}
