5963ffc4138e46bb30ac2a36dc3dbd3725980d1a
G0RYNYCH <92940275+GORYNTCHdotNET@users.noreply.github.com>
2023-03-06 17:22:18 +0300

    add entities

--- ProfilesAPI/Models/Receptionist.cs
+++ ProfilesAPI/Models/Receptionist.cs
@@ -1,10 +1,9 @@
 ﻿using System.Diagnostics.CodeAnalysis;
-using Microsoft.AspNetCore.Identity;
 
 namespace ProfilesAPI.Models;
 
 [ExcludeFromCodeCoverage]
-public class Doctor //: IdentityUser<Guid>
+public class Receptionist
 {
     public Guid Id { get; set; }
     
@@ -14,15 +13,7 @@ public class Doctor //: IdentityUser<Guid>
 
     public string? MiddleName { get; set; }
 
-    public DateOnly DateOfBirth { get; set; }
+    public Guid AcccountId{ get; set; }
 
-    public Guid AccountId { get; set; }
-    
-    public Guid SpeciallizationId { get; set; }
-    
     public Guid OfficeId { get; set; }
-
-    public int CareerStartYear { get; set; }
-
-    public string Status { get; set; }
 }
